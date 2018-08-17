4c07da54dcbd27b0ee3d36db5e121f0465695ed2
Krzysztof Gacek <grzysztof92@gmail.com>
2018-08-17 21:30:07 +0200

    Added material design module

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { MaterialDesignModule } from './material-design/material-design.module';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  exports: [
+    MaterialDesignModule
+  ]
 })
 export class SharedModule { }
