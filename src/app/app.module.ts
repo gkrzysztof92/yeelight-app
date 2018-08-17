59c0660b59a1a2179ba082a02837b4ab2c826b59
Krzysztof Gacek <grzysztof92@gmail.com>
2018-08-17 21:01:53 +0200

    Added material design dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -10,6 +11,7 @@ import { CoreModule } from './core/core.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CoreModule
   ],
   providers: [],
