e84669d760ad0f817ea7c49af8c4c50cb233e296
Krzysztof Gacek <grzysztof92@gmail.com>
2018-08-17 19:30:15 +0200

    Added discavery devices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, AfterViewInit {
+    
+  devices = [];
+
+  constructor(private electronService: ElectronService) { }
+
+  ngOnInit(): void {
+    this.electronService.ipcRenderer.on('asynchronous-reply', (event, arg) => {
+      if (this.devices.filter(dev => dev.id = arg.id).length === 0) {
+        this.devices.push(arg);
+      }
+      console.log(this.devices);
+    });
+  }
+
+  ngAfterViewInit(): void {
+    this.getDevices();
+  }
+
+  getDevices() {
+    this.electronService
+      .ipcRenderer.send('discavery-devices');
+  }
+  
 }
