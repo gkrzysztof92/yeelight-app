38fdf5525374590868290cabc79cb53976ed0f0f
Krzysztof Gacek <grzysztof92@gmail.com>
2018-08-20 23:41:13 +0200

    Implemented toggle device functionality

--- src/app/device/device/device.component.ts
+++ src/app/device/device/device.component.ts
@@ -4,6 +4,8 @@ import { Device } from '../../../yeelight-api/model/device';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 
 import { switchMap, map } from 'rxjs/operators';
+import { MatSlideToggleChange } from '@angular/material';
+import { Command } from '../../../yeelight-api/model/command';
 
 
 @Component({
@@ -26,6 +28,13 @@ export class DeviceComponent implements OnInit {
         .pipe(map(devices => devices.find(device => device.id === params.get('id'))))
         .subscribe(device => this.device = device);
     });
+  }
+
+  toggleDevice() {
+    const cmd = { deviceIp: this.device.location,  
+      commandPayload: { id: 1, method: 'toggle', params: [] }
+    } as Command;  
+    this.deviceService.sendCommand(cmd);
   }
 
 }
