8b7d8b7e282e13f5e4d4f1b7a479ca7c67767b78
Krzysztof Gacek <grzysztof92@gmail.com>
2018-08-22 22:36:55 +0200

    Added ngrx store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { StoreModule } from '@ngrx/store'
+import { StoreDevtoolsModule } from '@ngrx/store-devtools'
+
+import { environment } from '../environments/environment.prod'
+
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { DeviceModule } from './device/device.module';
@@ -16,7 +21,13 @@ import { DeviceModule } from './device/device.module';
     BrowserAnimationsModule,
     CoreModule,
     DeviceModule,
-    RouterModule.forRoot([])
+    RouterModule.forRoot([]),
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      name: 'Yeelight App DevTools',
+      maxAge: 25,
+      logOnly: environment.production
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
