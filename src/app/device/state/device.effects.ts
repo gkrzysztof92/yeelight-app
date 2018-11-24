c97f04f4d822acb5863487a2958820cfdc90788f
krzgac <krzysztof.gacek@fideltronik.com>
2018-11-24 21:30:50 +0100

    Fixed issue with unknow action payload  property

--- src/app/device/state/device.effects.ts
+++ src/app/device/state/device.effects.ts
@@ -1,10 +1,12 @@
 import { Injectable } from '@angular/core';
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { DevicesService } from 'src/app/core/services/devices.service';
 import { of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { DevicesService } from 'src/app/core/services/devices.service';
 
 import * as fromActions from './device.actions';
-import { tap, map, switchMap } from 'rxjs/operators';
+
 
 @Injectable()
 export class DeviceEffects {
@@ -20,7 +22,7 @@ export class DeviceEffects {
     toggleDevice$ = this.actions$
         .pipe(
           ofType(fromActions.DeviceActionTypes.DeviceCommand),
-          switchMap(action => {
+          switchMap((action: fromActions.DeviceCommand) => {
             this.devicesService.sendCommand(action.payload);
             return of({});
           })
